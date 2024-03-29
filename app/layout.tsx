735c5d2897b1a95e7d56f220e3f8174ee8045517
Suparida <jik2002x@gmail.com>
2024-03-29 23:18:57 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppWrapper } from "./context/supabase-context";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import { Head } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] });
 
